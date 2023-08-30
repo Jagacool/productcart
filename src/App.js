fa8c6838f637dc899704b88679057e7407ee6712
j <137029997+Jagacool@users.noreply.github.com>
2023-08-30 07:32:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import SmartphoneCategories from './components/SmartphoneCategories'; // Update the import
+import SmartphoneCategories from './components/SmartphoneCategories';
 import Products from './components/Products';
 import Cart from './components/Cart';
 import filterList from './components/filterList';
 
 const App = () => {
   const [products, setProducts] = useState([]);
-  const [selectedCategories, setSelectedCategories] = useState([]); // Update state name
+  const [selectedCategories, setSelectedCategories] = useState([]);
   const [cart, setCart] = useState([]);
 
   useEffect(() => {
@@ -18,26 +18,47 @@ const App = () => {
   }, []);
 
   const setCategory = (category) => {
-    const categories = [...selectedCategories]; // Update variable name
-
+    const categories = [...selectedCategories];
     if (categories.includes(category)) {
       const index = categories.indexOf(category);
       categories.splice(index, 1);
     } else {
       categories.push(category);
     }
-
     setSelectedCategories(categories);
-    setProducts(filterList(categories, 'category')); // Update filter parameter
+    setProducts(filterList(categories, 'category'));
   };
 
-  // Rest of your code remains the same
+  const sortProducts = (method) => {
+    const array = [...products]; // Make a copy of the products array
+
+    if (method === 'Lowest to Highest') {
+      array.sort(function (a, b) {
+        return a.price - b.price;
+      });
+    } else if (method === 'Highest to Lowest') {
+      array.sort(function (a, b) {
+        return b.price - a.price;
+      });
+    }
+    setProducts(array);
+  };
+
+  const addToCart = (item) => {
+    const productList = [...cart];
+    // Rest of your addToCart logic
+  };
+
+  const changeQuantity = (item, e) => {
+    const productList = [...cart];
+    // Rest of your changeQuantity logic
+  };
 
   return (
     <div className="App">
-      <SmartphoneCategories // Update component name
-        selectedCategories={selectedCategories} // Update prop name
-        setCategory={setCategory} // Update prop name
+      <SmartphoneCategories
+        selectedCategories={selectedCategories}
+        setCategory={setCategory}
       />
       <Products products={products} sortProducts={sortProducts} addToCart={addToCart} />
       <Cart products={cart} changeQuantity={changeQuantity} />
