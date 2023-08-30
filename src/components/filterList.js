3c587d9d87eab52a9eb78d4dd9eb38a60a3ccb1b
j <137029997+Jagacool@users.noreply.github.com>
2023-08-30 06:58:47 +0530

    Update filterList.js

--- src/components/filterList.js
+++ src/components/filterList.js
@@ -1,21 +1,16 @@
-import products from '../products.json'
+import products from '../products.json';
 
 export default function filterList(arr, method) {
-
-    if(method == null) return products;
-
-    else {
-          return products.filter(product => {
-          const sizeArray = product.size.split(" ");
-          if(arr.length > 0) {
-                if(sizeArray.some(r => arr.indexOf(r) >= 0)) {
-                    return product;
+    if (method == null) {
+        return products;
+    } else {
+        return products.filter(product => {
+            const sizeArray = product.size.split(" ");
+            if (arr.length > 0) {
+                return sizeArray.some(r => arr.indexOf(r) >= 0);
+            } else {
+                return true; // Always return true if no conditions are met
             }
-          }
-          else {
-              return products;
-          }
-      })  
+        });
     }
 }
-
